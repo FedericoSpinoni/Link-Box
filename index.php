60139d1a60c789e0fcf4de383c89cb4d1097e16e
Spino <federicospinoni@yahoo.it>
2018-03-31 21:55:21 +0200

    Layout
    Simple responsive layout

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('includes/head.php') ?>
+    
+    <div class="container">
+        <div class="slide">
+            
+        </div>
+    </div>
+
+<?php include('includes/foot.php') ?>
